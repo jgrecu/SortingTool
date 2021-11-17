f750e808f7cdbb1646239405dff4d3a01ebdc3ae
Jeremy Grecu <jeremy.grecu@sap.com>
2021-11-17 15:42:17 +0000

    JBA - Sorting Tool - Stage 1/6: Numbers only

--- task/src/sorting/Main.java
+++ task/src/sorting/Main.java
@@ -5,10 +5,16 @@ import java.util.*;
 public class Main {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Long> list = new ArrayList<>();
 
         while (scanner.hasNextLong()) {
             long number = scanner.nextLong();
-            // write your code here
+            list.add(number);
         }
+
+        System.out.printf("Total numbers: %d.%n", list.size());
+        long maxLong = list.stream().mapToLong(v -> v).max().orElse(0);
+        long countMax = list.stream().filter(v -> v == maxLong).count();
+        System.out.printf("The greatest number: %d (%d time(s)).%n", maxLong, countMax);
     }
 }
