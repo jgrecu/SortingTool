8312ff960d255605ec65d3d8a6e68cee236476e2
Jeremy Grecu <jeremy.grecu@sap.com>
2021-11-22 10:41:13 +0000

    JBA - Sorting Tool - Stage 6/6: X-files

--- task/src/sorting/Main.java
+++ task/src/sorting/Main.java
@@ -1,11 +1,16 @@
 package sorting;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class Main {
+    private static File outFile = null;
+    private static File inFile = null;
+
     public static void main(final String[] args) {
         parseArgs(args);
-
     }
 
     private static void parseArgs(String[] args) {
@@ -15,7 +20,10 @@ public class Main {
         arguments.stream().filter(s -> s.startsWith("-"))
                 .filter(s -> !s.equals("-sortingType"))
                 .filter(s -> !s.equals("-dataType"))
+                .filter(s -> !s.equals("-inputFile"))
+                .filter(s -> !s.equals("-outputFile"))
                 .forEach(s -> System.out.println("\"" + s + "\"" + " is not a valid parameter. It will be skipped."));
+
         if (arguments.contains("-sortingType")) {
             int sortingTypeIndex = arguments.indexOf("-sortingType") + 1;
             if (sortingTypeIndex >= arguments.size() ||
@@ -27,12 +35,32 @@ public class Main {
                 byCount = true;
             }
         }
+
+        if (arguments.contains("-outputFile")) {
+            int outFileIndex = arguments.indexOf("-outputFile") + 1;
+            if (outFileIndex >= arguments.size()) {
+                System.out.println("No output file name given!");
+            } else {
+                String outFileName = arguments.get(outFileIndex);
+                outFile = new File(outFileName);
+            }
+        }
+
+        if (arguments.contains("-inputFile")) {
+            int inFileIndex = arguments.indexOf("-inputFile") + 1;
+            if (inFileIndex >= arguments.size()) {
+                System.out.println("No output file name given!");
+            } else {
+                String inFileName = arguments.get(inFileIndex);
+                inFile = new File(inFileName);
+            }
+        }
+
         if (arguments.contains("-dataType")) {
             int dataTypeIndex = arguments.indexOf("-dataType") + 1;
             if (dataTypeIndex >= arguments.size() || !"long".equals(arguments.get(dataTypeIndex)) &&
                     !"line".equals(arguments.get(dataTypeIndex)) && !"word".equals(arguments.get(dataTypeIndex))) {
                 System.out.println("No data type defined!");
-                return;
             } else if ("long".equals(arguments.get(dataTypeIndex))) {
                 parseLong(byCount);
             } else if ("line".equals(arguments.get(dataTypeIndex))) {
@@ -47,6 +75,14 @@ public class Main {
 
     private static void parseLong(boolean byCount) {
         Scanner scanner = new Scanner(System.in);
+        if (inFile != null) {
+            try {
+                scanner = new Scanner(inFile);
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found");
+                System.out.println(e.getMessage());
+            }
+        }
         List<Long> list = new ArrayList<>();
         while (scanner.hasNext()) {
             String number = scanner.next();
@@ -73,11 +109,26 @@ public class Main {
             Collections.sort(list);
             list.forEach(i -> System.out.print(i + " "));
         }
+        if (outFile != null) {
+            try (PrintWriter printWriter = new PrintWriter(outFile)) {
+                printWriter.println("Total numbers: " + list.size());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
         scanner.close();
     }
 
     private static void parseLine(boolean byCount) {
         Scanner scanner = new Scanner(System.in);
+        if (inFile != null) {
+            try {
+                scanner = new Scanner(inFile);
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found");
+                System.out.println(e.getMessage());
+            }
+        }
         List<String> list = new ArrayList<>();
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
@@ -98,11 +149,26 @@ public class Main {
             Collections.sort(list, Comparator.comparing(String::length).reversed());
             list.forEach(System.out::println);
         }
+        if (outFile != null) {
+            try (PrintWriter printWriter = new PrintWriter(outFile)) {
+                printWriter.println("Total lines: " + list.size());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
         scanner.close();
     }
 
     private static void parseWords(boolean byCount) {
         Scanner scanner = new Scanner(System.in);
+        if (inFile != null) {
+            try {
+                scanner = new Scanner(inFile);
+            } catch (FileNotFoundException e) {
+                System.out.println("File not found");
+                System.out.println(e.getMessage());
+            }
+        }
         List<String> list = new ArrayList<>();
         while (scanner.hasNextLine()) {
             String[] line = scanner.nextLine().split("\\s+");
@@ -124,6 +190,13 @@ public class Main {
             System.out.print("Sorted data: ");
             Collections.sort(list);
             list.forEach(i -> System.out.print(i + " "));
+        }
+        if (outFile != null) {
+            try (PrintWriter printWriter = new PrintWriter(outFile)) {
+                printWriter.println("Total words: " + list.size());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
         scanner.close();
     }
